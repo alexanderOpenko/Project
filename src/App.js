5d80e91e9956bea6f3f3ab460b180dcf10fd657b
alexanderOpenko <openkooleksandr@gmail.com>
2022-06-06 23:31:02 +0300

    Create Collection component;  Connect php API to get collections with products

--- src/App.js
+++ src/App.js
@@ -1,54 +1,78 @@
-import React from 'react'
+import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
-import FrontPage from './Components/FrontPage/FrontPage';
+import $ from "jquery";
 import Header from './Components/Header/header';
 import Navbar from './Components/Navbar/Navbar';
-import News, {newItemsPhoto} from './Components/News/News';
-import Jeans from './Components/Jeans/JeansContainer';
-import Hoody, {hoodyPhoto} from './Components/Hoody/hoody';
-import PageContainer from './Components/Page/PageContainer';
 import Footer from './Components/footer/footer';
-import {jeansPhoto} from './Components/FrontPage/FrontPageSale/FrontPageSale';
-import Tshirts, {tshirtPhoto} from './Components/Tshirts/Tshirts';
-import Clothes, {clothesPhotoArray} from './Components/clothes/clothes'
-import Accessories, {accessoriesPhoto} from './Components/Accessories/Accessories'
+import Collection from "./Components/collection/Collection";
+import {connect} from "react-redux";
+import {routesCreator} from './Redux-reducers/navigationReducer';
+import ProductCreate from "./Components/ProductCreate";
 
-const App = (props) => {
-  document.body.style.cssText = `font-family: "Graphik Cond Web"; overflow-x: hidden`
-  {
-    return (
+class App extends React.Component {
+    componentDidMount() {
+        document.body.style.cssText = `font-family: "Graphik Cond Web"; overflow-x: hidden`
 
-      <div className='wrapper-content'>
+        const promise1 = new Promise((resolve, reject) => $.ajax({
+            type: "GET",
+            url: "http://localhost:8888/collection.php",
+            data: "collection",
+            header: 'Content-Type: application/json',
+            success (data) {
+                resolve(data)
+            },
+        })
+        )
 
-        <Header/>
+        promise1.then((value) => {
+           const data = JSON.parse(value);
+           //this.props.routesCreator(data);
+           console.log(data, 'data')
+           // console.log(value, 'value')
+        });
+    }
 
-        <div className='frontpage-content'>
-          <Navbar/>
-          <Switch>
-            <Route exact path='/' render={() => <FrontPage/>}/>
+    render() {
+        return (
+            <div className='wrapper-content'>
 
-            <Route path={'/sale/saleJeans/:id'} render={(props) => (<PageContainer photos={jeansPhoto} {...props}/>)}/>
-            <Route path={'/newItems/:id'} render={(props) => (<PageContainer photos={newItemsPhoto} {...props}/>)}/>
-            <Route path={'/hoody/:id'} render={(props) => (<PageContainer photos={hoodyPhoto} {...props}/>)}/>
-            <Route path={'/tshirt/:id'} render={(props) => (<PageContainer photos={tshirtPhoto} {...props}/>)}/>
-            <Route path={'/clothes/:id'} render={(props) => (<PageContainer photos={clothesPhotoArray} {...props}/>)}/>
-            <Route path={'/Accessories/:id'}
-                   render={(props) => (<PageContainer photos={accessoriesPhoto} {...props}/>)}/>
-            <Route path='/jeans' render={() => <Jeans/>}/>
-            <Route path='/clothes' render={() => <Clothes/>}/>
-            <Route path='/new' render={() => <News/>}/>
-            <Route path='/hoody' render={() => <Hoody/>}/>
-            <Route path='/tshirt' render={() => <Tshirts/>}/>
-            <Route path='/Accessories' render={() => <Accessories/>}/>
-          </Switch>
+                <Header/>
 
-        </div>
-        <div className='footer'>
-          <Footer/>
-        </div>
-      </div>
-    )
-  }
+                <div className='frontpage-content'>
+                    <Navbar/>
+                    <Switch>
+                        {/*<Route exact path='/' render={() => <FrontPage/>}/>*/}
+
+                        {/*<Route path={'/sale/saleJeans/:id'} render={(props) => (<PageContainer photos={jeansPhoto} {...props}/>)}/>*/}
+                        {/*<Route path={'/newItems/:id'} render={(props) => (<PageContainer photos={newItemsPhoto} {...props}/>)}/>*/}
+                        {/*<Route path={'/hoody/:id'} render={(props) => (<PageContainer photos={hoodyPhoto} {...props}/>)}/>*/}
+                        {/*<Route path={'/tshirt/:id'} render={(props) => (<PageContainer photos={tshirtPhoto} {...props}/>)}/>*/}
+                        {/*<Route path={'/clothes/:id'} render={(props) => (<PageContainer photos={clothesPhotoArray} {...props}/>)}/>*/}
+                        {/*<Route path={'/Accessories/:id'} render={(props) => (<PageContainer photos={accessoriesPhoto} {...props}/>)}/>*/}
+
+                        {
+                            this.props.routes.map(el => {
+                            return <Route path={"/" + el} render={(props) => <Collection {...props}/>}/>
+                        })
+                        }
+
+                        <Route path={'/admin'} render={(props) => <ProductCreate {...props}/>}/>
+                    </Switch>
+
+                </div>
+                <div className='footer'>
+                    <Footer/>
+                </div>
+            </div>
+        )
+    }
 }
-export default App;
+
+const mapStateToProps = (state) => {
+    return({
+        routes: state.navigation.routes
+    })
+}
+
+export default connect(mapStateToProps, {routesCreator})(App)
